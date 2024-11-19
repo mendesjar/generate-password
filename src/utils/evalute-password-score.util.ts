6c3bb2c7ff4a498dd2eb2d07e74a525d48bac89b
mendesjar <ga0301@outlook.com>
2024-11-19 14:48:47 -0300

    fix: bg util evalute passord score and remove score

--- src/utils/evalute-password-score.util.ts
+++ src/utils/evalute-password-score.util.ts
@@ -3,29 +3,34 @@ export function evaluatePasswordStrength(password: string) {
   if (!password) return "";
   // Check password length
   if (password.length > 8) score += 1;
+  else score -= 1;
   // Contains lowercase
   if (/[a-z]/.test(password)) score += 1;
+  else score -= 1;
   // Contains uppercase
   if (/[A-Z]/.test(password)) score += 1;
+  else score -= 1;
   // Contains numbers
   if (/\d/.test(password)) score += 1;
+  else score -= 1;
   // Contains special characters
   if (/[^A-Za-z0-9]/.test(password)) score += 1;
+  else score -= 1;
 
   switch (score) {
     default:
-      return "Short";
+      return "Short bg-red-800";
     case 0:
-      return "Short";
+      return "Short bg-red-800";
     case 1:
-      return "Weak";
+      return "Weak bg-red-500";
     case 2:
-      return "Weak";
+      return "Weak bg-red-200";
     case 3:
-      return "Medium";
+      return "Medium bg-yellow-500";
     case 4:
-      return "Medium";
+      return "Medium bg-yellow-200";
     case 5:
-      return "Strong";
+      return "Strong bg-green-800";
   }
 }
